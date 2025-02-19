a854554a88389c119254e87164c6174be82f6297
Alexsey Arkhipov <lekha.arkhipov.2003@mail.ru>
2025-02-19 23:35:02 +0300

    DZ4 API

--- modules/textTransform.js
+++
@@ -1,4 +0,0 @@
-// modules/textTransform.js
-export function transformText(str, searchValue, replaceValue) {
-    return str.replaceAll(searchValue, replaceValue);
-}
--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// script.js
-import { comments, updateTasks } from './modules/comments.js'; // Один массив комментариев
-import { renderComments } from './modules/renderComments.js';
-import { addEventHandlers } from './modules/eventHandlers.js';
+import { addCommemt, getDataUserComments } from "./modules/functions.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    renderComments(comments); // Отображаем комментарии при загрузке
-    addEventHandlers(comments, renderComments); // Передаем массив комментариев и рендер-функцию
-});
-// script.js
-fetch('https://wedev-api.sky.pro/api/v1/alex-arkhipov/comments')
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        updateComments(data.comments)
-        renderUserComments()
-    })
+addCommemt();
+getDataUserComments();
