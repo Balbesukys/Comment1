43f74951a768b3c16f55421243ecdd645fe58cb8
Alexsey Arkhipov <lekha.arkhipov.2003@mail.ru>
2025-02-28 15:08:13 +0300

    Исправил ДЗ

--- modules/htmlUtils.js
+++
@@ -1,7 +0,0 @@
-// modules/htmlUtils.js
-
-export const sanitizeHtml = (value) => {
-    const div = document.createElement('div');
-    div.textContent = value; // Используем textContent для предотвращения XSS
-    return div.innerHTML; // Возвращаем безопасный HTML
-};
